b7874c81987ef28c21858b864cccfa398e1de935
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-20 21:29:17 +0530

    Node-local-server

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-const userName = "Mridul";
+// // Globaly avaliable function to import api's
+// const fs = require('fs');
 
-console.log(userName);
+
+// const userName = "Mridul";
+
+
+// console.log(userName);
+// // alert(userName) Not defined
+
+// // FS file system is async function hence requires callback
+
+// fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("Wrote in file");
+// });
+
+// To create webserver
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("Incomming request");
+    console.log(req.method, req.url);
+
+    // Have to send a res because the browser will throw and error when it times out
+    res.end("Success!")
+
+});
+
+server.listen(5000);
